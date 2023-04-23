b83536c6d65d3641acd648f5dcf8c17374bbe0a1
rollwagen <rollwagen@gmail.com>
2023-04-23 13:30:54 +0200

    Introduce separate run command

--- cmd/root.go
+++ cmd/root.go
@@ -17,27 +17,14 @@ package cmd
 
 import (
 	"os"
-	"strings"
 
-	apoch "github.com/rollwagen/apoch/pkg"
 	"github.com/spf13/cobra"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-)
-
-var (
-	logLevel string
-	skipScan bool
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "apoch",
 	Short: "Run a port scan of all VPC public IPs found in AWS ConfigService",
-	Run: func(cmd *cobra.Command, args []string) {
-		logger := setupLogger(logLevel)
-		apoch.QueryIPsAndScan(logger, skipScan)
-	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,34 +34,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	rootCmd.Flags().StringVar(&logLevel, "loglevel", "info", "Select log level [\"debug\", \"info\", \"warn\", \"error\"]")
-	rootCmd.Flags().BoolVar(&skipScan, "noscan", false, "Only discover public IPs and skip port scanning")
-}
-
-func setupLogger(logLevel string) *zap.SugaredLogger {
-	zapLogLevel := zapcore.InfoLevel
-
-	switch strings.ToUpper(logLevel) {
-	case "ERROR":
-		zapLogLevel = zapcore.ErrorLevel
-	case "WARN":
-		zapLogLevel = zapcore.WarnLevel
-	case "DEBUG":
-		zapLogLevel = zapcore.DebugLevel
-	}
-
-	encoderCfg := zapcore.EncoderConfig{
-		MessageKey:  "message",
-		LevelKey:    "level",
-		TimeKey:     "time",
-		EncodeLevel: zapcore.CapitalColorLevelEncoder,
-	}
-
-	encoder := zapcore.NewConsoleEncoder(encoderCfg)
-	core := zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), zapLogLevel)
-
-	return zap.New(core).Sugar()
 }
